470412b4253d31f586a516d708249491a9726fe7
ArthurHermes <arthurfhermes@gmail.com>
2024-06-06 21:24:27 -0300

    Creating

---
+++ Java.Vscode/Sisteminha/conexao/ConexaoBancoDados.java
@@ -0,0 +1,52 @@
+package conexao;
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class ConexaoBancoDados {
+    
+    String url = "jdbc:mysql://localhost:3306/biblioteca";
+    String user = "root";
+    String password = "";
+
+
+    public void consultarLivros() {
+        try {
+            Connection connection = DriverManager.getConnection(url, user, password);
+            System.out.println("Conexão bem-sucedida!");
+
+            String sql = "SELECT * FROM Usuario";
+            PreparedStatement statement = connection.prepareStatement(sql);
+
+            ResultSet resultSet = statement.executeQuery();
+
+            while (resultSet.next()) {
+                int idUsuario = resultSet.getInt("idUsuario");
+                String nome = resultSet.getString("nome");
+                String senha = resultSet.getString("senha");
+                String endereco = resultSet.getString("endereco");
+                int tipoUsuario = resultSet.getInt("tpUsuario");
+
+                System.out.println(
+                        "id: " + idUsuario + 
+                        "\nNome: " + nome +
+                        "\nSenha: " + senha + 
+                        "\nendereco" + endereco +
+                        "\nTipo Usuario: " + tipoUsuario);
+            }
+
+            connection.close();
+        } catch (SQLException e) {
+            System.out.println("Erro ao conectar ao banco de dados: " + e.getMessage());
+        }
+    }
+
+    public static void main(String[] args) {
+        ConexaoBancoDados conexao = new ConexaoBancoDados();
+        conexao.consultarLivros();
+    }
+}
--- Java.Vscode/Sisteminha/livro.java
+++ Java.Vscode/Sisteminha/livro.java
@@ -1,3 +1,101 @@
 public class livro {
-    
+    private int idLivro;
+    private String nome;
+    private Float valor;
+    private int numPag;
+    private categoria categoria;
+    private classificacao classificacao;
+
+
+    private enum categoria{
+        romance,
+        ficcaoCientifica,
+        fantasia,
+        misterio,
+        suspense,
+        horror,
+        biografia,
+        historia,
+        programacao,
+        gastronomia
+    }
+
+    private enum classificacao{
+        infantil,
+        livre
+    }
+
+    private String idioma;
+
+    private boolean disponibilidade;
+
+
+    /* Constructor */
+
+    public livro(int idLivro, String nome, Float valor, int numPag, categoria categoria, classificacao classificacao, String idioma, boolean disponibilidade){
+        this.idLivro = idLivro;
+        this.nome = nome;
+        this.valor = valor;
+        this.numPag = numPag;
+        this.categoria = categoria;
+        this.classificacao = classificacao;
+        this.idioma = idioma;
+        this.disponibilidade = disponibilidade;
+    }
+
+
+    /* Setters */
+    public void setIdLivro(int idLivro) {
+        this.idLivro = idLivro;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public void setValor(Float valor) {
+        this.valor = valor;
+    }
+    public void setNumPag(int numPag) {
+        this.numPag = numPag;
+    }
+    public void setCategoria(categoria categoria) {
+        this.categoria = categoria;
+    }
+    public void setClassificacao(classificacao classificacao) {
+        this.classificacao = classificacao;
+    }
+    public void setIdioma(String idioma) {
+        this.idioma = idioma;
+    }
+    public void setDisponibilidade(boolean disponibilidade) {
+        this.disponibilidade = disponibilidade;
+    }
+
+
+    /* Getters */
+
+    public int getIdLivro() {
+        return idLivro;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public Float getValor() {
+        return valor;
+    }
+    public int getNumPag() {
+        return numPag;
+    }
+    public categoria getCategoria() {
+        return categoria;
+    }
+    public classificacao getClassificacao() {
+        return classificacao;
+    }
+    public String getIdioma() {
+        return idioma;
+    }
+    public boolean getDisponibilidade(){
+        return disponibilidade;
+    }
+
 }
